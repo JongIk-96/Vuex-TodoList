9147b0cdc5e039b1dd3c0292b70609d51536a84e
박종익 <pji3504@naver.com>
2021-06-23 14:05:32 +0900

    vue.js todolist 초기상태

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped> 
+/* scoped : 상위 html요소의 style을 아래로 상속받게 되는걸 무시한다. */
+h1 {
+    color: #2F3852;
+    font-weight : 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
